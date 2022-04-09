46ab73a9f09306945ea9d0b2bebd714e7e41c873
Filippo <filippo.pisello@live.it>
2022-04-09 14:10:51 +0200

    docs + refactor: fix docs and turn in dataclass

--- spreadpandas/spreadsheet_element.py
+++ spreadpandas/spreadsheet_element.py
@@ -1,45 +1,46 @@
-"""Contain the class to describe a portion of a spreadsheet"""
+"""Class to describe a portion of a spreadsheet"""
 from __future__ import annotations
+
+from dataclasses import dataclass
 
 from . import operations
 from .custom_types import Cells, CellsRange, CoordinatesPair
 
 
+@dataclass
 class SpreadsheetElement:
     """
     Class to describe a rectangular portion of a spreadsheet.
 
-    ---------------
     Identify a rectangular part of a spreadsheet by providing a pair of
-    coordinates that identify that top left and bottom right corner of the
+    coordinates that represent the top left and bottom right corner of the
     rectangle.
 
-    The object is useful to access the various forms in which the rectangle
+    This object allows to access the various forms in which the rectangle
     of cells can be identified (cells, coordinates, cells_range).
 
-    Arguments
-    ----------------
-    coordinates: list in the form [(int, int), (int, int)], (mandatory)
-        A list containing two tuples of length two, each containing two numbers
-        which univocally identify a cell. The two numbers are respectively the
-        row index and the column number. The two cells are the top left and
+    Parameters
+    ----------
+    coordinates : tuple[tuple[int, int], tuple[int, int]]
+        A tuple containing two tuples of length two, each containing two numbers
+        which univocally identify a cell. The two numbers are indexes of the
+        respectively the columns and rows. The two cells are the top left and
         bottom right one.
     """
 
-    def __init__(self, coordinates: CoordinatesPair):
-        self.coordinates = coordinates
+    coordinates: CoordinatesPair
 
     @property
     def cells(self) -> Cells:
         """
-        Returns tuple of cells making up the object in the form ("A1", "A2").
+        Return tuple of cells making up the object in the form ("A1", "A2").
         """
         return operations.cells_rectangle(self.coordinates)
 
     @property
     def cells_range(self) -> CellsRange:
         """
-        Returns a str giving info on the top left and bottom right cells of the
+        Return a str giving info on the top left and bottom right cells of the
         object, in the form "A1:B3".
         """
         return operations.cells_range(self.coordinates)
